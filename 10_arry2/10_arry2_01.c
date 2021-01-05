6c88c0ba82f017940fe6324acf9b9d6ee4120aad
036ab@naver.com <31887989+minsuck@users.noreply.github.com>
2021-01-05 15:25:01 +0900

    2차원 배열의 출력

---
+++ 10_arry2/10_arry2_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int arr[2][4] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8}
+	};
+
+	for (int i=0; i<2; i++){
+		for (int j=0; j<4; j++){
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+  return 0;
+}
