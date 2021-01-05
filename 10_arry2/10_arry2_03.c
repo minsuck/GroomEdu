ee46084c48c1224687f0972c7a7e4602de7a3a50
036ab@naver.com <31887989+minsuck@users.noreply.github.com>
2021-01-05 15:25:32 +0900

    i, j에 들어갈 올바른 수는?

---
+++ 10_arry2/10_arry2_03.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i = 0;
+    int j = 0;
+
+    int tarr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    printf("%d", tarr[i][j]);
+    return 0;
+}
